05c61ea0340491c97e392883294f68669d78d141
NIRANJAN CREATIONS <hackerenvironment1514@gmail.com>
2025-11-01 11:48:36 +0530

    add.py

---
+++ add.py
@@ -0,0 +1,5 @@
+import tkinter 
+from tkinter import simpledialog,messagebox
+th=simpledialog.askinteger("KNI-ORGANISATION","ENTER HOUR:\n")
+tm=simpledialog.askinteger("KNI-ORGANISATION","ENTER MINUTE:\n")
+ts=simpledialog.askinteger("KNI-ORGANISATION","ENTER SECOND:\n")
